762df1c14b6c78c6c5cfc2f6a9109195f6573484
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-10-24 15:17:35 +0330

    fix/bug in contact-us

--- src/components/ContactUs/ContactUsContent/ContactUsContent.tsx
+++ src/components/ContactUs/ContactUsContent/ContactUsContent.tsx
@@ -10,11 +10,11 @@ export interface IContactResponse{
 
 const ContactUsContent = () => {
   return (
-    <div className='max-w-[100%] w-full flex flex-col sm:flex-row mt-20'>
-        <div className='max-w-[60%] w-full'>
+    <div className='max-w-[100%] w-full flex flex-col lg:flex-row mt-20'>
+        <div className='lg:max-w-[60%] w-full'>
         <RightSide />
         </div>
-        <div className='max-w-[40%] w-full'>
+        <div className='lg:max-w-[40%] lg:mt-0 mt-10 w-full'>
             <ContactForm action={ContactUsFetch} />
         </div>
     </div>
