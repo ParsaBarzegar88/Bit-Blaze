26358424d56d483a4b2caf5fc028f9a9f3c52220
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-10-30 22:16:09 +0330

    feat:implement dashboard page

---
+++ src/app/(private)/(buyerDashboard)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: "داشبورد",
+};
+const DashboardPage = () => {
+  return (
+    <>
+      {/* <BuyerDashboard /> */}
+    </>
+  )
+}
+
+export default DashboardPage
---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,34 @@
+import localFont from 'next/font/local'
+import "./globals.css";
+import TokenRefresher from '@/utils/refreshToken';
+import { CookiesProvider } from 'next-client-cookies/server';
+export const PeydaFanum = localFont({
+  src: [
+    {
+      path: "../../assets/Fonts/IRANSansXFaNum-Medium.ttf",
+      weight: "500"
+    }
+  ]
+})
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${PeydaFanum.className} bg-[#ffffff] antialiased dark:bg-[#232323] min-h-screen overflow-x-hidden flex justify-center`}
+      >
+        <CookiesProvider>
+          {children}
+          <TokenRefresher />
+        </CookiesProvider>
+      </body>
+    </html>
+  );
+}
+
+
+
