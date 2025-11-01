6d729e3f9ec34c716972d352423ca14cd289ac74
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-11-01 23:29:14 +0330

    feat:implement some change in dashboard

--- src/components/Dashboard/BuyerDashboard/DashboardPageSection/DashboardStatusProfile/DashboardProfileInformation/DashboardProfileInformation.tsx
+++ src/components/Dashboard/BuyerDashboard/DashboardPageSection/DashboardStatusProfile/DashboardProfileInformation/DashboardProfileInformation.tsx
@@ -4,10 +4,10 @@ import { FaBookmark } from 'react-icons/fa6'
 import ArrowLeftSVG from '../../../.././BuyerDashboardSVG/arrowLeftSVG';
 import { IUserDetail } from '@/core/types/LandingPage/IUserDetail';
 
-interface IProps{
+interface IProps {
     userInfo: IUserDetail
 }
-const DashboardProfileInformation:FC<IProps> = ({userInfo}) => {
+const DashboardProfileInformation: FC<IProps> = ({ userInfo }) => {
     return (
         <div className='
             bg-white dark:bg-[#363636]
@@ -34,17 +34,17 @@ const DashboardProfileInformation:FC<IProps> = ({userInfo}) => {
                     </Link>
                 </div>
                 <div className='w-full mt-2 mb-2 border-t border-gray-300 dark:border-white border-dashed transition-colors duration-300'></div>
-                <div className='mt-8 flex flex-row justify-between items-center'>
+                <div className='mt-8 flex  sm:flex-row flex-col justify-between items-center'>
                     <div className='flex flex-col gap-2'>
                         <span className='text-[36px] text-black dark:text-white'>{userInfo.additionalPercentage}%</span>
                         <p className='text-[16px] max-w-[70%] text-wrap text-black dark:text-white'>برای اینکه بازدید خوبی داشته باشید، پروفایل شما باید حداقل ۷۰٪ تکمیل شده باشد.</p>
                     </div>
                     <div className='relative w-[150px] h-[150px] flex justify-center items-center'>
-                    <div className='absolute inset-0 rounded-full bg-[#D9D9D9] dark:bg-gray-700' />
-                    <div
-                        className='absolute inset-0 rounded-full'
-                        style={{
-                            background: `conic-gradient(
+                        <div className='absolute inset-0 rounded-full bg-[#D9D9D9] dark:bg-gray-700' />
+                        <div
+                            className='absolute inset-0 rounded-full'
+                            style={{
+                                background: `conic-gradient(
                                     from 0deg,
                                     #8CFF45 0%,
                                     #4ADE80 ${userInfo.additionalPercentage * 0.8}%,
@@ -52,17 +52,17 @@ const DashboardProfileInformation:FC<IProps> = ({userInfo}) => {
                                     transparent ${userInfo.additionalPercentage}%,
                                     transparent 100%
                                 )`,
-                            mask: `radial-gradient(transparent 55px, black 56px)`,
-                            WebkitMask: `radial-gradient(transparent 55px, black 56px)`,
-                            transition: 'all 0.7s ease-out',
-                        }}
-                    />
-                    <div className='absolute inset-0 flex justify-center items-center'>
-                        <div className='bg-white dark:bg-[#1F2937] w-[110px] h-[110px] rounded-full flex flex-col justify-center items-center shadow-inner'>
-                            <span className='text-2xl font-bold text-black dark:text-white'></span>
+                                mask: `radial-gradient(transparent 55px, black 56px)`,
+                                WebkitMask: `radial-gradient(transparent 55px, black 56px)`,
+                                transition: 'all 0.7s ease-out',
+                            }}
+                        />
+                        <div className='absolute inset-0 flex justify-center items-center'>
+                            <div className='bg-white dark:bg-[#1F2937] w-[110px] h-[110px] rounded-full flex flex-col justify-center items-center shadow-inner'>
+                                <span className='text-2xl font-bold text-black dark:text-white'></span>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
