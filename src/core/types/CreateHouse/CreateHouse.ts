bf0a857806002766f449c103efe35e390bf740e7
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-11-16 19:44:20 +0330

    feat: create type of CreateHouse Section

--- src/core/types/CreateHouse/CreateHouse.ts
+++ src/core/types/CreateHouse/CreateHouse.ts
@@ -1,3 +1,21 @@
 export interface CreateHouse {
-    
+title: string,
+  address: string,
+  rate: number,
+  price: number,
+  tags: string[],
+  capacity: number,
+  location: {
+    lat: number,
+    lng: number
+  },
+categories: {
+    name: string
+  },
+  bathrooms: number,
+  parking: number,
+  rooms: number,
+  yard_type: string,
+  transaction_type: string,
+  "caption": string | null
 }
