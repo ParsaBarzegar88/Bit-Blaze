435ca10ce8a6573e0f21e9ce032eeccf549ec87e
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-11-16 23:21:35 +0330

    feat:implement step three anf four design

--- src/components/Dashboard/SellerDashboard/DashboardHouseMangementPageSection/CreateHouse/MultiSteps/step3/StepThree.tsx
+++ src/components/Dashboard/SellerDashboard/DashboardHouseMangementPageSection/CreateHouse/MultiSteps/step3/StepThree.tsx
@@ -2,11 +2,31 @@ import React from 'react'
 
 const StepThree = () => {
   return (
-    <div className='flex flex-col w-full gap-3.5 '>
-      <div className='flex flex-row justify-between w-full gap-4'>
-        <input type="text" placeholder='test'/>
-        <input type="text" placeholder='test'/>
+    <div className='flex flex-col w-full gap-10.5 '>
+      <div className='flex flex-col md:flex-row  justify-between w-full gap-15'>
+        <fieldset className='w-full border rounded-[16px] border-[#AAAAAA] items-center flex pb-2'>
+          <legend className='mr-2 pr-2 pl-2 text-[#AAAAAA]'>تعداد اتاق : </legend>
+          <input type="text" className='w-full max-w-[98%] text-black mx-auto outline-none border-none' />
+        </fieldset>
+        <fieldset className='w-full border rounded-[16px] border-[#AAAAAA] items-center flex pb-2'>
+          <legend className='mr-2 pr-2 pl-2 text-[#AAAAAA]'>تعداد حمام : </legend>
+          <input type="text" className='w-full max-w-[98%] text-black mx-auto outline-none border-none' />
+        </fieldset>
       </div>
+      <div className='flex flex-col md:flex-row justify-between w-full gap-15'>
+        <fieldset className='w-full border rounded-[16px] border-[#AAAAAA] items-center flex pb-2'>
+          <legend className='mr-2 pr-2 pl-2 text-[#AAAAAA]'>تعداد پارکینگ : </legend>
+          <input type="text" className='w-full max-w-[98%] text-black mx-auto outline-none border-none' />
+        </fieldset>
+        <fieldset className='w-full border rounded-[16px] border-[#AAAAAA] items-center flex pb-2'>
+          <legend className='mr-2 pr-2 pl-2 text-[#AAAAAA]'>نوع حیاط : </legend>
+          <input type="text" className='w-full max-w-[98%] text-black mx-auto outline-none border-none' />
+        </fieldset>
+      </div>
+      <fieldset className='w-full border rounded-[16px] border-[#AAAAAA] items-center flex pb-2'>
+          <legend className='mr-2 pr-2 pl-2 text-[#AAAAAA]'>برچسب ها : </legend>
+          <input type="text" className='w-full max-w-[98%] text-black mx-auto outline-none border-none' />
+        </fieldset>
     </div>
   )
 }
