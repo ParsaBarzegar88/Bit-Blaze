efac2556c25301717f014ba981712b865067e84f
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-11-29 01:43:55 +0330

    bug-fix

--- src/app/(public)/house-reserve/[id]/page.tsx
+++ src/app/(public)/house-reserve/[id]/page.tsx
@@ -1,27 +1,24 @@
-import HouseReserveDetail from '@/components/HouseReserve/Detail/Detail'
-import React, { FC } from 'react'
+import HouseReserveDetail from '@/components/HouseReserve/Detail/Detail';
 import { SendCommentFetch } from '@/core/api/HouseReserve/Detail/SendComments';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: "رزرو خانه",
 };
-interface IProps {
-    params: {
-        id: string
-    }
-}
 export interface ISendCommentsResponse {
   error?: string;
   success?: boolean
 }
-const HouseReserveDetailPage: FC<IProps> = ({params}) => {
+export default async function HouseReserveDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
 
   return (
     <div>
-       <HouseReserveDetail DetailId={params.id} action={SendCommentFetch} />
+       <HouseReserveDetail DetailId={id} action={SendCommentFetch} />
     </div>
   )
 }
-
-export default HouseReserveDetailPage
--- src/app/(public)/mortgage-rent/[id]/page.tsx
+++ src/app/(public)/mortgage-rent/[id]/page.tsx
@@ -1,26 +1,23 @@
 import MortgageRentDetailSection from '@/components/MortgageRent/MortgageRentDetailSection/MortgageRentDetailSection';
 import { SendCommentFetch } from '@/core/api/MortgageRent/MortgageRentDetail/SendComments';
 import { Metadata } from 'next';
-import React, { FC } from 'react'
 export const metadata: Metadata = {
   title: "رهن و اجاره",
 };
 
-interface IProps{
-    params: {
-        id: string;
-    };
-}
 export interface ISendCommentsResponse {
   error?: string;
   success?: boolean
 }
-const page:FC<IProps> = ({params}) => {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
   return (
     <>
-      <MortgageRentDetailSection houseId={params.id} action={SendCommentFetch} />
+      <MortgageRentDetailSection houseId={id} action={SendCommentFetch} />
     </>
   )
 }
-
-export default page
