1326939d89d86014f78771f5ccc9bee83ff7de81
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-11-29 01:13:43 +0330

    bug-fix

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,19 +1,11 @@
-import localFont from 'next/font/local'
-import "./globals.css";
 import Footer from '@/components/Footer/Footer';
-import ToggleDarkAndLightProvider from './ThemeProvider';
-import { FooterFetch } from '@/core/api/Footer/Footer';
-import { CookiesProvider } from 'next-client-cookies/server';
-import TokenRefresher from '@/utils/refreshToken';
 import MainHeader from '@/components/Header/MainHeader';
-export const PeydaFanum = localFont({
-  src: [
-    {
-      path: "../../assets/Fonts/IRANSansXFaNum-Medium.ttf",
-      weight: "500"
-    }
-  ]
-})
+import { FooterFetch } from '@/core/api/Footer/Footer';
+import { PeydaFanum } from '@/utils/fonts';
+import TokenRefresher from '@/utils/refreshToken';
+import { CookiesProvider } from 'next-client-cookies/server';
+import "./globals.css";
+import ToggleDarkAndLightProvider from './ThemeProvider';
 export interface IFooterResponse {
   error?: string;
   success?: boolean
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,9 +1,7 @@
 import localFont from 'next/font/local'
 export const PeydaFanum = localFont({
-  src: [
-    {
-      path: "../../../assets/Fonts/IRANSansXFaNum-Medium.ttf",
-      weight: "500"
-    }
-  ]
+  src: '../assets/fonts/IRANSansXFaNum-Medium.ttf',
+  weight: '500',
+  variable: '--font-peyda',
+  display: 'swap',
 })
